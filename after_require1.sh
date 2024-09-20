2b96f9496e3334e3c5cbc5e0d001d9b3f76528ab
ctchuan27 <145923933+ctchuan27@users.noreply.github.com>
2024-09-21 02:22:07 +0800

    Update after_require1.sh

--- after_require1.sh
+++ after_require1.sh
@@ -1,3 +1,3 @@
 pip install -e .
 pip uninstall torch torchvision
-pip install torch==2.1.1+cu121 torchvision==0.16.1+cu121 -f https://download.pytorch.org/whl/torch_stable.html
+pip install torch==1.8.0+cu111 torchvision==0.9.0+cu111 -f https://download.pytorch.org/whl/torch_stable.html
